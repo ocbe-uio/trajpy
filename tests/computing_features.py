80c38c8b75f2a7baf684fb9ab64992ade86916bc
Mauricio Moreira <phydev@users.noreply.github.com>
2019-10-11 16:21:19 +0100

    indent

--- tests/computing_features.py
+++ tests/computing_features.py
@@ -1,8 +1,8 @@
 import sys
+
 sys.path.insert(0, '../trajpy')
 import trajpy as tj
 import unittest
-
 
 
 class TestFeatures(unittest.TestCase):
@@ -12,13 +12,14 @@ class TestFeatures(unittest.TestCase):
             test if the efficiency is being computed correctly for two extreme cases
             1) linear trajectory and 2) random steps.
         """
-      x1 = np.linspace(0, 100, 100)
-      x2 = np.random.rand(100)
-    
-      r = tj.Trajectory()
-      
-      self.assertAlmostEqual(r.efficiency_(x1), 1.0 )
-      self.assertAlmostEqual(r.efficiency_(x2), 0.0 )
+
+        x1 = np.linspace(0, 100, 100)
+        x2 = np.random.rand(100)
+
+        r = tj.Trajectory()
+
+        self.assertAlmostEqual(r.efficiency_(x1), 1.0)
+        self.assertAlmostEqual(r.efficiency_(x2), 0.0)
 
 
 if __name__ == '__main__':
