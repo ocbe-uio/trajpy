873465279db270482e515c10be4e23f6586afc4e
phydev <mmoreirauc@gmail.com>
2019-10-16 13:28:13 +0100

    latex correction

--- trajpy/traj_generator.py
+++ trajpy/traj_generator.py
@@ -6,7 +6,7 @@ def weierstrass_mandelbrot(t, n_displacements, gamma=np.sqrt(np.pi), alpha=.8):
     calculates the weierstrass mandelbrot function
     
     .. math::
-        W(t) = \\sum_{-\\infty}^{\\infty} \\frac{\\cos{(\\phi_n )} - \\cos{(\\gamma^n t^* + \\phi_n )} }{\\gamma^{n\\alpha/2}}
+        W(t) = \\sum_{-\\infty}^{n = \\infty} \\frac{\\cos{(\\phi_n )} - \\cos{(\\gamma^n t^* + \\phi_n )} }{\\gamma^{n\\alpha/2}}
 
     :param t: time step
     :param n_displacements: number of displacements
