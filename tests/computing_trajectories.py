96d15814277126d7b89bd88f44840d0a7699d411
Mauricio Moreira <phydev@users.noreply.github.com>
2020-01-03 16:11:40 +0000

    test superdiffusion

--- tests/computing_trajectories.py
+++ tests/computing_trajectories.py
@@ -10,7 +10,7 @@ class TestTrajectoryGenerator(unittest.TestCase):
     def setUpClass(cls):
         np.random.seed(0)
 
-    def test_anomalous_trajectory(self):
+    def test_anomalous_diffusion(self):
         """
         Test if the generator of anomalous trajectories is working properly
         by measuring the fractal dimension.
@@ -25,7 +25,7 @@ class TestTrajectoryGenerator(unittest.TestCase):
 
         fractal_dimension, d_max = r.fractal_dimension_(trajectory)
 
-        self.assertAlmostEqual(fractal_dimension, 3.08, places=1)
+        self.assertAlmostEqual(fractal_dimension, 3.0, places=1)
 
     def test_normal_diffusion(self):
         """
@@ -35,15 +35,52 @@ class TestTrajectoryGenerator(unittest.TestCase):
         n_steps = 250
         n_samples = 1
         dt = 0.1
+        D = 100.0
         
-        xa, trajectory = tjg.normal_diffusion(n_steps, n_samples, 1.0, 0., 100, dt)
+        xa, trajectory = tjg.normal_diffusion(n_steps, n_samples, 1.0, 0., D, dt)
         
         r = tj.Trajectory()
         
         fractal_dimension, d_max = r.fractal_dimension_(trajectory)
         
-        self.assertAlmostEqual( np.around(fractal_dimension, decimals=0), 2.0, places=1)
+        self.assertAlmostEqual(np.around(fractal_dimension, decimals=0), 2.0, places=1)
         
-
+    def test_confined_diffusion(self):
+        """
+        Test if the generator of confined diffusion trajectories is working properly
+        by measuring the fractal dimension.
+        """
+        n_steps = 250
+        n_samples = 1
+        dt = 0.1
+        D = 100.0
+        radius = 10.
+        
+        xa, trajectory = tjg.confined_diffusion(radius, n_steps, n_samples, 1.0, 0., D, dt)
+        
+        r = tj.Trajectory()
+        
+        fractal_dimension, d_max = r.fractal_dimension_(trajectory)
+        
+        self.assertAlmostEqual(np.around(fractal_dimension, decimals=0), 3.0, places=1)
+        
+     def test_superdiffusion(self):
+        """
+        Test if the generator of superdiffusion trajectories is working properly
+        by measuring the fractal dimension.
+        """
+        n_steps = 250
+        n_samples = 1
+        dt = 0.1
+        velocity = 1.0
+        
+        xa, trajectory = tjg.superdiffusion(velocity, n_steps, n_samples, 0., dt)
+        
+        r = tj.Trajectory()
+        
+        fractal_dimension, d_max = r.fractal_dimension_(trajectory)
+        
+        self.assertAlmostEqual(np.around(fractal_dimension, decimals=0), 1.0, places=1)  
+        
 if __name__ == '__main__':
     unittest.main()
