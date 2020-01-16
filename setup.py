b0c71c331c0c7b7b8580a60b6ff7b7940944fa94
phydev <mmoreirauc@gmail.com>
2020-01-16 17:27:51 +0000

    increase version in setup.py to 0.9.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='trajpy',
-    version='0.0.1',
+    version='0.9.0',
     url='https://github.com/phydev/trajpy.git',
     author='Mauricio Moreira',
     author_email='mms@uc.pt',
