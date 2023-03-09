af2b5eea636030405138f2dbe969e37982fc27d1
Maurício Moreira-Soares <phydev@users.noreply.github.com>
2023-03-09 10:42:41 +0100

    Update tests/computing_features.py

--- tests/computing_features.py
+++ tests/computing_features.py
@@ -5,7 +5,7 @@ import sys
 import os
 src_path = (os.path.abspath(os.path.join(os.path.dirname(__file__), '..')) + '/trajpy/')
 sys.path.append(src_path)
-from trajpy import trajpy as tj
+import trajpy.trajpy as tj
 
 class TestFeatures(unittest.TestCase):
  
